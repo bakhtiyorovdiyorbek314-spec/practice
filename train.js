04985790e3ad00b0c4fbdc39c1c614bed00e5917
Diyorbek <bakhtiyorovdiyorbek314@gmail.com>
2026-09-12 13:46:50 +0900

    feat: TASK F is completed

--- train.js
+++ train.js
@@ -1,3 +1,29 @@
+//TASK F
+// Yagona string argumentga ega findDoublers nomli function tuzing
+// Agar stringda bittadan ortiq bir xil harflar ishtirok etgan bo'lsa
+// true yokida false natija qaytarsin.
+
+// MASALAN: findDoublers("hello"); natija true qaytadi. Sababi ikki marotaba takrorlangan 'll' harfi mavjud!
+
+function findDoublers(a) {
+  const nechta = {};
+
+  for (i = 0; i < a.length; i++) {
+    const letter = a[i];
+    if (nechta[letter]) {
+      //oldin uchragan bolsa
+      return true;
+    }
+    nechta[letter] = true; //korilgani
+  }
+  return false;
+}
+bormi = findDoublers("hello");
+console.log("bormi:", bormi);
+
+bormi = findDoublers("fruit");
+console.log("bormi:", bormi);
+
 //TASK-E
 // Shunday function tuzing, u bitta string argumentni qabul qilib osha stringni teskari qilib return qilsin.
 // MASALAN: getReverse("hello") return qilsin "olleh"
