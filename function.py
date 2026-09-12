b19ad4006ee34316472e3119894e4acd07d49cc9
Diyorbek <bakhtiyorovdiyorbek314@gmail.com>
2026-09-12 12:26:11 +0900

    fix: learn Keyword and default arguments

--- function.py
+++ function.py
@@ -28,3 +28,26 @@ print("result1:", result1)   # void function bolgani uchun none qaytardi
 
 result2 = greeting('Steve')
 print('result2', result2)
+
+
+print("==== Keyword vs Default argument =====")
+# Define
+
+
+def give_greet(name, age=25):
+    print("give_greet is executed ")
+    return f"My name is {name} and i am {age} years old"
+
+
+# CALL
+result3 = give_greet("Deen", 24)
+print('result3:', result3)   # bu oddiy agrument berish usuli
+
+result4 = give_greet(name="Steve", age=24)
+# bu KEYWORD argument berish usuli, call qismida yozing , oqilishi oson bolishi uchun kk
+print('result4:', result4)
+
+
+# bu holda age kiritilmagani uchun tepadan DEFAULT qilib oldi
+result5 = give_greet("John")
+print('result5:', result5)
