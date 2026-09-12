ba944627ee5eb544b3d0b0b1577ef5ee47178beb
Diyorbek <bakhtiyorovdiyorbek314@gmail.com>
2026-09-12 11:15:51 +0900

    feat: learn sting in python

--- primitive.py
+++ primitive.py
@@ -9,3 +9,21 @@ print(f"the coumt :{count} and type :{count_type}")
 result1 = count.bit_count()  # method
 result2 = count.numerator  # state
 print(result1, result2)
+
+
+print("===== string =====")
+# Method: upper() lower() title() find() replace()
+
+course = "AI Python Fullstack"
+
+result = type(course)
+print(f"the result(1):{result}")
+
+result = course.upper()
+print(f"the result(2):{result}")
+
+result = course.title()
+print(f"the result(3):{result}")
+
+result = course.replace("Fullstack", "MasterClass")
+print(f"the result(4):{result}")
