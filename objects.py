345ebe7e6fdffb527a9b0cf6a5866cb186ea26f5
Diyorbek <bakhtiyorovdiyorbek314@gmail.com>
2026-09-13 23:11:20 +0900

    feat: handle specific and all kind of Errors

--- objects.py
+++ objects.py
@@ -25,3 +25,68 @@ result1 = math.ceil(66.4)  # CALL - math object ning ceil methodi
 result2 = ceil(33.2)
 print("result1:", result1)
 print("result2:", result2)
+
+print("===== Error handling system ======")
+car_dict = dict(name="BMW", year=2026, electric=True)
+
+# (1)XATOLIK BOLGANDA:
+
+try:
+    print("passed here")
+    # bu yerda mavjud bolmagani uchun except ishga tushyapti
+    result1 = car_dict["origin"]
+    print("result1:", result1)
+except KeyError as err:
+    print("NO origin state property found:", err)
+else:
+    print("Executed succesfully without errors")
+finally:
+    print("Final closing logic")
+
+
+# (2)XATOLIK BOLMAGANDA:
+
+try:
+    print("passed here")
+    # mavjud bolgani uchun  else ishga tushyapti
+    result1 = car_dict["year"]
+    print("result1:", result1)
+except KeyError as err:
+    print("NO origin state property found:", err)
+else:
+    print("Executed succesfully without errors")
+finally:
+    print("Final closing logic")
+
+
+# (3) BIR NECHTA XATOLIK TEKSHIRILGANDA:
+
+try:
+    print("passed here")
+    # mavjud bolgani uchun  else ishga tushyapti
+    result1 = car_dict["year"]
+    a = car_dict.speed
+    print("result1:", result1)
+except (KeyError, AttributeError) as err:  # except qismiga shunday yoz
+    print("ERROR:", err)
+else:
+    print("Executed succesfully without errors")
+finally:
+    print("Final closing logic")
+
+
+# (4) HAR QANDAY XATOLIKKA TEKSHIRILGANDA:
+
+
+try:
+    print("passed here")
+    # mavjud bolgani uchun  else ishga tushyapti
+    result1 = car_dict["year"]
+    a = car_dict.speed
+    print("result1:", result1)
+except Exception as err:  # except qismiga shunday yoz
+    print(" general ERROR:", err)
+else:
+    print("Executed succesfully without errors")
+finally:
+    print("Final closing logic")
